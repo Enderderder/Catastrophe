a2a3b7dc56e2e432e4c834860183e1b5535b454f
James <jjohnstone2520@gmail.com>
2019-08-16 14:56:52 +1200

    ~Added item crate actor class ~Added yarnball crate ~Added disguise crate

---
+++ Source/Catastrophe/Interactable/ItemCrate.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ItemCrate.generated.h"
+
+UCLASS()
+class CATASTROPHE_API AItemCrate : public AActor
+{
+	GENERATED_BODY()
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* ItemMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UStaticMeshComponent* OutlineMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UBoxComponent* TriggerVolume;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
+	class UInteractableComponent* InteractableComponent;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "ItemSack")
+	TSubclassOf<class AItemSack> ItemSackType;
+
+public:	
+	// Sets default values for this actor's properties
+	AItemCrate();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void PickupItem(class APlayerCharacter* _playerCharacter);
+	UFUNCTION(BlueprintImplementableEvent, Category = "ItemCrate", meta = (DisplayName = "OnPickupItem"))
+	void Receive_PickupItem();
+};
