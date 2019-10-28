f72628b2488f8d236c3afa927f856fb54d1388dc
Enderderder <richardwulansari@gmail.com>
2019-10-28 21:17:25 +1300

    ~ Fixed player cannot aim after cave issue ~ Added ventilation shortcut class

---
+++ Source/Catastrophe/Gameplay/HideoutGameplay/VentilationShortcut.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VentilationShortcut.h"
+
+#include "Components/BoxComponent.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/ArrowComponent.h"
+
+#include "Interactable/BaseClasses/InteractableComponent.h"
+#include "Characters/PlayerCharacter/PlayerCharacter.h"
+
+// Sets default values
+AVentilationShortcut::AVentilationShortcut()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	SceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("SceneRoot"));
+	RootComponent = SceneRoot;
+
+	TriggerBox = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerBox"));
+	TriggerBox->SetCanEverAffectNavigation(false);
+	TriggerBox->SetGenerateOverlapEvents(true);
+	TriggerBox->SetCollisionProfileName(TEXT("Trigger"));
+	TriggerBox->SetupAttachment(RootComponent);
+
+	PopVentMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PopVentMesh"));
+	PopVentMesh->SetCanEverAffectNavigation(false);
+	PopVentMesh->SetGenerateOverlapEvents(false);
+	PopVentMesh->SetCollisionProfileName(TEXT("BlockAll"));
+	PopVentMesh->SetupAttachment(RootComponent);
+
+	TeleportTransformComponent = CreateDefaultSubobject<USceneComponent>(TEXT("TeleportTransformComponent"));
+	TeleportTransformComponent->SetupAttachment(RootComponent);
+
+	TeleportArrow = CreateDefaultSubobject<UArrowComponent>(TEXT("TeleportArrow"));
+	TeleportArrow->SetupAttachment(TeleportTransformComponent);
+
+	InteractComponent = CreateDefaultSubobject<UInteractableComponent>(TEXT("InteractComponent"));
+	InteractComponent->RegisterTriggerVolume(TriggerBox);
+	InteractComponent->OnPlayerEnterInteractRange.RemoveDynamic(this, &AVentilationShortcut::OnPlayerEnterInteractRange);
+	InteractComponent->OnPlayerEnterInteractRange.AddDynamic(this, &AVentilationShortcut::OnPlayerEnterInteractRange);
+	InteractComponent->OnInteractSuccess.RemoveDynamic(this, &AVentilationShortcut::OnInteractSuccess);
+	InteractComponent->OnInteractSuccess.AddDynamic(this, &AVentilationShortcut::OnInteractSuccess);
+}
+
+void AVentilationShortcut::OnPlayerEnterInteractRange(class APlayerCharacter* _playerCharacter)
+{
+	Receive_ShowHint(_playerCharacter, HintText);
+}
+
+void AVentilationShortcut::OnInteractSuccess(class APlayerCharacter* _playerCharacter)
+{
+	if (bLocked)
+	{
+		Receive_ShowHint(_playerCharacter, HintTextWhenLocked);
+	}
+	else
+	{
+		FVector teleportLocation = TeleportTransformComponent->GetComponentLocation();
+		FRotator teleportRotation = TeleportTransformComponent->GetComponentRotation();
+		_playerCharacter->SetActorLocationAndRotation(teleportLocation, teleportRotation);
+	}
+}
+
+// Called when the game starts or when spawned
+void AVentilationShortcut::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
